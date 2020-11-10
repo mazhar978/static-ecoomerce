3f8109ac9198de8ca70618cc72b12dbd6e8ddbdd
mazhar978 <74258715+mazhar978@users.noreply.github.com>
2020-11-10 08:48:20 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var button = document.getElementById('button')
+var button2 = document.getElementById('button2')
+var button3 = document.getElementById('button3')
+var button4 = document.getElementById('button4')
+var image = document.getElementById('image')
+function color(){
+    button.style.backgroundColor = "orange"
+    button2.style.backgroundColor = "red"
+    button3.style.backgroundColor = "orange"
+    button4.style.backgroundColor = "blue"
+}
+function change(){
+    button.style.backgroundColor = "unset"
+    button2.style.backgroundColor = "unset"
+    button3.style.backgroundColor = "unset"
+    button4.style.backgroundColor = "unset"
+    
+}
